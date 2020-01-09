6f6f88e83bf77ed252f8ffe1d573e0e690c4aa06
garrettfreshwater-nss <gfresh.nss@gmail.com>
2020-01-09 10:45:42 -0600

    added departments to the json database

--- scripts/EmployeeList.js
+++ scripts/EmployeeList.js
@@ -1,6 +1,7 @@
 
 import { useComputers } from "./ComputerDataProvider.js";
 import { useEmployees } from "./EmployeeDataProvider.js";
+import { useDepartments } from "./DepartmentProvider.js";
 import Employees from "./EmployeeComponents.js";
 
 
@@ -10,16 +11,22 @@ export const EmployeeList = () => {
 
     const employees = useEmployees()
     const computers = useComputers()
+    const departments = useDepartments()
 
     const render = () => {
         contentTarget.innerHTML = employees.map(employee => { //each employee is iterated over. 
+
             // Find this employees computer 
             const computer = computers.find(computer => computer.id === employee.computerId)
 
+            // Find this employees computer 
+            const department = departments.find(department => department.id === employee.departmentId)
+
             // Get HTML representation of employees
-            const html = Employees(employee, computer)
+            const html = Employees(employee, computer, department)
 
             return html
+
         }).join("")
     }
 
--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,10 @@
 import { getEmployees } from "./EmployeeDataProvider.js";
 import { getComputers } from "./ComputerDataProvider.js";
 import EmployeeList from "./EmployeeList.js";
+import { getDepartments } from "./DepartmentProvider.js";
 
 
 getEmployees()
     .then(getComputers)
+    .then(getDepartments)
     .then(EmployeeList)
